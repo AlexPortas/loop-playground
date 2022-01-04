8debc8f5a4af783a827db99b8610ae354cfa0db5
Alex Portas <alexalonsoportas@gmail.com>
2022-01-04 14:50:19 +0100

    ex2 feito

--- ex2.js
+++ ex2.js
@@ -2,3 +2,8 @@
  * Ejercicio 2: Escribe un bucle, parecido al del ejercicio 1, 
  * que muestre por el terminal el mensaje "Yo también estoy en un bucle!" ; un total de 20 veces
  */
+
+
+ for (var i=0; i<28; i++) {
+    console.log("Yo tambien estoy en un bucle! " + (i+1))
+}
